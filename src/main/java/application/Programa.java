b6c78e99757e9a99f0b4bf637c7dc02928d7f00a
Julio Cesar <julio.si.freitas@gmail.com>
2020-10-12 21:13:31 -0300

    Metodos find e remove

--- src/main/java/application/Programa.java
+++ src/main/java/application/Programa.java
@@ -9,17 +9,18 @@ import javax.persistence.Persistence;
 public class Programa {
 
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(null, "Julio Cesar", "julio@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Jorge Jesus", "jj@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Hugo Moura", "hm@gmail.com");
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
+
+        Pessoa p = em.find(Pessoa.class, 2);
+
         em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
+        em.remove(p);
         em.getTransaction().commit();
+
         System.out.println("Pronto!");
+        em.close();
+        emf.close();
     }
 }
